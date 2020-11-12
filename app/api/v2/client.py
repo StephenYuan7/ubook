c60ef4adaea30d584a99022cf15a8e4f335b23d3
stephen yuan <2583242079@qq.com>
2020-11-12 08:41:17 +0800

    更改test的methods

--- app/api/v2/client.py
+++ app/api/v2/client.py
@@ -4,6 +4,6 @@ from app.libs.red_print import Redprint
 api = Redprint('client')
 
 
-@api.route('/test', methods=['POST'])
+@api.route('/test', methods=['GET'])
 def check_client():
     return 'this is a test'
